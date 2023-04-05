6e55a07735fcd1924a138b4af610d1b882d3255e
mmti <mmshooreshi@gmail.com>
2023-04-05 03:54:34 +0330

    s

--- pages/tweet-gen.vue
+++ pages/tweet-gen.vue
@@ -195,7 +195,7 @@ export default defineComponent({
             }
 
             if (error?.value) {
-                errMsg.value = error.value.data.error.message
+                errMsg.value = error.value.data
             }
 
             loading.value = false

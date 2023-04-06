842a241d00835b15406c0188b06daeebdc3b293e
mmti <mmshooreshi@gmail.com>
2023-04-06 12:28:53 +0330

    s

--- server/api/test.post.ts
+++ server/api/test.post.ts
@@ -1,1 +1,15 @@
-export default defineEventHandler(() => 'Test post handler')
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event)
+  
+
+
+  // const id = parseInt(event.context.params.id) as number
+  // if (!Number.isInteger(id)) {
+  //   throw createError({
+  //     statusCode: 400,
+  //     statusMessage: 'ID should be an integer',
+  //   })
+  // }
+  return { body }
+
+})

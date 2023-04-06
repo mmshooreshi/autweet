9193d1e503e70a7d83c660120003838dd8102ac0
mmti <mmshooreshi@gmail.com>
2023-04-06 12:41:36 +0330

    s

--- server/api/test.post.ts
+++ server/api/test.post.ts
@@ -1,6 +1,28 @@
+import { Configuration, OpenAIApi } from "openai";
+const config = useRuntimeConfig()
+const configuration = new Configuration({
+  apiKey: config.chatgpt.apiKey,
+});
+
+const openai = new OpenAIApi(configuration);
+
+
 export default defineEventHandler(async (event) => {
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "Hello world" }],
+  });
+
+
   const body = await readBody(event)
-  
+
+  const key = config.chatgpt.apiKey
+  return completion.data.choices[0].message
+
+
+})
+
 
 
   // const id = parseInt(event.context.params.id) as number
@@ -10,6 +32,3 @@ export default defineEventHandler(async (event) => {
   //     statusMessage: 'ID should be an integer',
   //   })
   // }
-  return { body }
-
-})

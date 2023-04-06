6baff0847070de424d358cb55e8fa27d76834eb4
mmti <mmshooreshi@gmail.com>
2023-04-06 13:55:59 +0330

    s

--- server/api/tweet-gen.post.ts
+++ server/api/tweet-gen.post.ts
@@ -9,7 +9,7 @@ export default defineEventHandler(async (event) => {
 
   var pre_prompt=""
   var tweetCount, topicsList, tones, sentiments, focusAreas, userPreferences, inputSamples, language
-  var completion
+  var completion: any
   const body = await readBody(event)
   try {
     tweetCount= body.tweetCount;topicsList= body.topicsList;tones=body.tones;sentiments=body.sentiments;focusAreas=body.focusAreas;userPreferences=body.userPreferences;inputSamples=body.inputSamples;language=body.language;
@@ -17,11 +17,13 @@ export default defineEventHandler(async (event) => {
   } catch (error) {
 
   }
+  pre_prompt = `"As a bot that generates tweets in ${language} language, your task is to create ${tweetCount} ${language} tweets covering a range of topics such as ${topicsList}. Incorporate various ${tones} tones and ${sentiments} sentiments to keep your tweets engaging and interesting. Use eye-catching hashtags, mentions, and emojis to grab the reader's attention. Ensure that each tweet is concise, yet within Twitter's 280-character limit. To make your tweets more effective, consider the ${focusAreas} and user preferences. To guide your creativity, use the following input samples: ${JSON.stringify(inputSamples)}. Please just respond with an Array of Objects, each object, as a tweet. Also have in mind, that the user-preferences for these tweets are as follows: ${JSON.stringify(userPreferences)}"`
+
   try{
-  pre_prompt = `"As a bot that generates tweets in ${language}, your task is to create ${tweetCount} tweets covering a range of topics such as ${topicsList}. Incorporate various ${tones} tones and ${sentiments} sentiments to keep your tweets engaging and interesting. Use eye-catching hashtags, mentions, and emojis to grab the reader's attention. Ensure that each tweet is concise, yet within Twitter's 280-character limit. To make your tweets more effective, consider the ${focusAreas} and user preferences. To guide your creativity, use the following input samples: ${inputSamples}. also have in mind, that the user-preferences for these tweets are as follows: ${userPreferences}"`
    completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: pre_prompt }],
+    max_tokens: 2500
   });
 }
 catch(error){
@@ -29,7 +31,7 @@ catch(error){
 }
 
 try{  
-const tweetsResponse:any =completion?.data.choices[0].message
+const tweetsResponse:any =completion?.data?.choices[0].message
   if(tweetsResponse.role=="assistant"){
     return tweetsResponse?.content
   }
@@ -37,8 +39,8 @@ const tweetsResponse:any =completion?.data.choices[0].message
 }catch(error){
 
 }
-  return  0
 
+  return  "error 202"
 
 })
 
@@ -51,3 +53,6 @@ const tweetsResponse:any =completion?.data.choices[0].message
   //     statusMessage: 'ID should be an integer',
   //   })
   // }
+
+
+  

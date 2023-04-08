1cf8d34e0067d4b88a354b03995a6ffc36abf8bc
mmti <mmshooreshi@gmail.com>
2023-04-08 06:19:09 +0330

    s

--- api/edge.ts
+++ api/edge.ts
@@ -29,15 +29,15 @@ export default async (event: any) => {
     userPreferences
   )}"`;
 
-  const response = await fetch("https://api.openai.com/v1/chat/completions", {
+  const response:any = await fetch("https://api.openai.com/v1/chat/completions", {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
     },
     method: "POST",
     body: JSON.stringify({
-      model: "text-davinci-002",
-      prompt: prePrompt,
+      model: "gpt-3.5-turbo",
+      messages: [{ "role": "user", "content": prePrompt }],
       max_tokens: 2000,
       stream: true,
       stop: ["\n\n"],
